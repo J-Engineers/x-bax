2a8175476dec31874d07a9cd6772bbd21be7a8da
unknown <ugboguj@yahoo.com>
2022-08-24 11:37:52 +0100

    Updated CORs

--- src/__init__.py
+++ src/__init__.py
@@ -27,7 +27,7 @@ S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(BUCKET_NAME)
 
 app_cors_config = {
     "origins": ['http://localhost:5000', 'http://e-services-app.herokuapp.com'],
-    "methods": ['GET', 'POST', 'PUT', 'DELETE'],
+    "methods": ['GET', 'POST', 'PUT', 'DELETE', 'OPTION'],
     "allow_headers": ['Authorization', 'Content-Type', 'accept']
 }
 
